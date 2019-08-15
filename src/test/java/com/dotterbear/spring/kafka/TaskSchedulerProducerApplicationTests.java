62e0629171fa78b7aa906d4b3c8455f08f3f6d38
kan01234 <safghjkl@gmail.com>
2019-08-15 21:50:36 +0800

    add docker-compose

--- src/test/java/com/dotterbear/spring/kafka/TaskSchedulerProducerApplicationTests.java
+++ src/test/java/com/dotterbear/spring/kafka/TaskSchedulerProducerApplicationTests.java
@@ -4,9 +4,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import com.dotterbear.task.scheduler.producer.TaskSchedulerProducerApplication;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = TaskSchedulerProducerApplication.class)
 public class TaskSchedulerProducerApplicationTests {
 
 	@Test
